8138734c6045cc83a64c5a964b4f4aff26c4c193
tototrinh <to.trinh@codeenginestudio.com>
2019-07-05 16:09:33 +0700

    BookServlet: Change the name of the attributes id, name, author to _id, _name, _author

--- src/net/codejava/model/Book.java
+++ src/net/codejava/model/Book.java
@@ -3,33 +3,33 @@ package net.codejava.model;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Book {
-	private static final AtomicInteger count = new AtomicInteger(0); 
-	private int id;
-	private String name, author;
+	private static final AtomicInteger _COUNT = new AtomicInteger(0); 
+	private int _id;
+	private String _name, _author;
 	
 	public int getId() {
-		return id;
+		return _id;
 	}
 	public void setId(int id) {
-		this.id = id;
+		this._id = id;
 	}
 	public String getName() {
-		return name;
+		return _name;
 	}
 	public void setName(String name) {
-		this.name = name;
+		this._name = name;
 	}
 	public String getAuthor() {
-		return author;
+		return _author;
 	}
 	public void setAuthor(String author) {
-		this.author = author;
+		this._author = author;
 	}
 	
 	public Book(String name,String author){
-		this.id = count.incrementAndGet();
-		this.name = name;
-		this.author = author;
+		this._id = _COUNT.incrementAndGet();
+		this._name = name;
+		this._author = author;
 		
 	}
 	
